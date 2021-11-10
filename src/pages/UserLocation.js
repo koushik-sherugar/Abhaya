7c9153b915b363816d2436d86610438b7530019d
kunal kumar <kunalkumar06022003@gmail.com>
2021-11-10 07:16:29 +0530

    feat: added userLocation route

--- src/pages/UserLocation.js
+++ src/pages/UserLocation.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
 const mapStyles = {
@@ -6,22 +6,45 @@ const mapStyles = {
   height: "100%",
 };
 
-
-export default function UserLocation() {
+function UserLocation(props) {
+  const [position, setposition] = useState({ lat: "", long: "" });
   useEffect(() => {
     if ("geolocation" in navigator) {
       console.log("Available");
       navigator.geolocation.getCurrentPosition(function (position) {
         console.log("Latitude is :", position.coords.latitude);
         console.log("Longitude is :", position.coords.longitude);
+        setposition({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
       });
     } else {
       console.log("Not Available");
     }
   }, []);
+
+  const onMarkerClick = () => {
+    console.log("clicked . . .");
+  };
+
   return (
     <div>
-      <h1>hello</h1>
+      <Map
+        google={props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: position.lat,
+          lng: position.long,
+        }}
+      >
+        <Marker onClick={onMarkerClick} name={"Current location"} />
+      </Map>
     </div>
   );
 }
+
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_MAPS_API_KEY,
+})(UserLocation);
