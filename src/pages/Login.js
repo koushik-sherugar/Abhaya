d4304289e9c916c2413236f10046d5ba161b3c4e
kunal kumar <kunalkumar06022003@gmail.com>
2021-11-12 20:34:06 +0530

    added login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,87 +1,82 @@
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    Stack,
-    
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
-  
-  export default function Login() {
-    return (
-      <Flex
-        minH={'100vh'}
-        align={'center'}
-        justify={'center'}
-        bg={useColorModeValue('gray.50', 'gray.800')}>
-        <Stack spacing={4} mx={'auto'} maxW={'lg'} py={12} px={2}>
-          <Stack align={'center'}>
-            <Heading fontSize={'4xl'}>Log in </Heading>
-            <Text fontSize={'lg'} color={'grey.500'}>
-              for the safety of you and your loved ones.
-            </Text>
-          </Stack>
-          <Box
-          minW={'70vh'}
-            rounded={'lg'}
-            bg={useColorModeValue('white', 'gray.700')}
-            boxShadow={'lg'}
-            p={2}>
-            <Stack spacing={4}>
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+export default function Login() {
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={4} mx={"auto"} maxW={"lg"} py={12} px={2}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"}>Log in </Heading>
+          <Text fontSize={"lg"} color={"grey.500"}>
+            for the safety of you and your loved ones.
+          </Text>
+        </Stack>
+        <Box
+          minW={"70vh"}
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={2}
+        >
+          <Stack spacing={4}>
             <FormControl id="name">
-                <FormLabel>enter your name</FormLabel>
-                <Input type="text" />
-              </FormControl>
-              <FormControl id="email">
-                <FormLabel>Email address</FormLabel>
-                <Input type="email" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>enter your password</FormLabel>
-                <Input type="password" />
-              </FormControl>
-              <FormControl id="contact1">
-                <FormLabel>Contact name</FormLabel>
-                <Input type="text" />
-              </FormControl>
-              <FormControl id="number1">
-                <FormLabel>number</FormLabel>
-                <Input type="number" />
-              </FormControl>
-              <FormControl id="contact2">
-                <FormLabel>Contact name</FormLabel>
-                <Input type="text" />
-              </FormControl>
-              <FormControl id="number2">
-                <FormLabel>number</FormLabel>
-                <Input type="number" />
-              </FormControl>
-              
-              <Stack spacing={10}>
-                {/* <Stack
+              <FormLabel>Name</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <FormControl id="contact1">
+              <FormLabel>Contact name</FormLabel>
+              <Input type="text" />
+            </FormControl>
+            <FormControl id="number1">
+              <FormLabel>Phone Number</FormLabel>
+              <Input type="number" required />
+            </FormControl>
+            {/* TODO: A meesage textArea which is required */}
+            <Stack spacing={10}>
+              {/* <Stack
                   direction={{ base: 'column', sm: 'row' }}
                   align={'start'}
                   justify={'space-between'}>
                   <Checkbox>Remember me</Checkbox>
                   <Link color={'blue.400'}>Forgot password?</Link>
                 </Stack> */}
-                <Button
-                  bg={'pink.400'}
-                  color={'white'}
-                  _hover={{
-                    bg: 'pink.500',
-                  }}>
-                  Sign in
-                </Button>
-              </Stack>
+              <Button
+                bg={"pink.400"}
+                color={"white"}
+                _hover={{
+                  bg: "pink.500",
+                }}
+                type="button"
+              >
+                Sign in
+              </Button>
             </Stack>
-          </Box>
-        </Stack>
-      </Flex>
-    );
-  }
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+}
